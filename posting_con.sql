7698274684ca9af64a395b95583d625c709bc764
prathyushapingili421 <prathyusha.ravinderreddy@gmail.com>
2024-11-26 17:26:12 -0800

    Delete posting_con.sql

--- posting_con.sql
+++
@@ -1,3 +0,0 @@
-COPY dev.public.postings FROM 's3://mydbproject/project/cleaned_postings.csv' IAM_ROLE 'arn:aws:iam::339712854011:role/service-role/AmazonRedshift-CommandsAccessRole-20241125T034843' FORMAT AS CSV DELIMITER ',' QUOTE '"' IGNOREHEADER 1 REGION AS 'us-east-2'
-
-select * from sys_load_error_detail
